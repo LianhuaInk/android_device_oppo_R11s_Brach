8d1f3fd711bef55f0154181ec05516f85b28525f
Cyzi7777 <m13045350508@163.com>
2022-07-05 16:15:31 +0800

    R11s: Kang Light HAL
    From LineageOS/android_device_xiaomi_sdm660-common

--- device.mk
+++ device.mk
@@ -81,6 +81,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
 
+# Light
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.oppo_sdm660
+    
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
